e91861b50ba677f776c93b0acdd6254b5e6c74cd
JuneSunAt7 <jprogram@bk.ru>
2023-09-11 18:04:12 +0300

    USER-CLIENT

--- server.go
+++ server.go
@@ -1,28 +1,56 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"net"
+	"os/user"
 )
 
-func main() {
+var getDict = map[int]string{
+	1: "GETINFO",
+	2: "GETSTAT",
+	3: "GETFILE",
+	4: "GETERROR",
+}
 
-	conn, err := net.Dial("tcp", "127.0.0.1:4545")
+var postDict = map[int]string{
+	1: "POSTINFO",
+	2: "POSTDATA",
+}
+
+func getusr() string {
+	user, err := user.Current()
+	if err != nil {
+		panic(err.Error())
+	}
+	return user.Username
+}
+func geths() {
+	nameUsr := getusr()
+	hashname := sha1.New()
+	hashname.Write([]byte(nameUsr))
+	sha1_hash := hex.EncodeToString(hashname.Sum(nil))
+
+	fmt.Println(sha1_hash)
+
+	conn, err := net.Dial("tcp", "127.0.0.1:9091")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
 	for {
-		var source = "1234"
+		var source = sha1_hash
 
-		// отправляем сообщение серверу
+		// отправляем название компьютера в виде хеша
 		if n, err := conn.Write([]byte(source)); n == 0 || err != nil {
 			fmt.Println(err)
 			return
 		}
 		// получем ответ
-		fmt.Print("Handshake hash:")
+		fmt.Print("Handshaked hash:")
 		buff := make([]byte, 1024)
 		n, err := conn.Read(buff)
 		if err != nil {
@@ -31,4 +59,8 @@ func main() {
 		fmt.Print(string(buff[0:n]))
 		fmt.Println()
 	}
+}
+func main() {
+	geths()
+
 }
--- web-interface/scripts/script.js
+++ web-interface/scripts/script.js
@@ -1,17 +1,6 @@
 
+
   function callback() {
 
-    let file = "tree.json";;
-
-    let reader = new FileReader();
-  
-    reader.readAsText(file);
-  
-    reader.onload = function() {
-      console.log(reader.result);
-    };
-  
-    reader.onerror = function() {
-      console.log(reader.error);
-    };
   };
+  callback();
