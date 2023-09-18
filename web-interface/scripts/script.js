0bf60dc771c221149586252f794a7b5a87618ccb
JuneSunAt7 <jprogram@bk.ru>
2023-09-18 23:32:21 +0300

    ADDED GOCLOUD TOKENS AND CREATE FOLDER

--- web-interface/scripts/script.js
+++ web-interface/scripts/script.js
@@ -4,3 +4,18 @@
 
   };
   callback();
+  let captionsList = document.querySelectorAll('.caption-item');
+let unitsList = document.querySelectorAll('.unit');
+
+captionsList.forEach(function (item, index) {
+  item.addEventListener('mouseover', function () {
+     unitsList[index].classList.add('hovered');
+  });
+  
+  item.addEventListener('mouseout', function () {
+     unitsList[index].classList.remove('hovered');
+  });
+});
+function showDash(){
+  
+}
