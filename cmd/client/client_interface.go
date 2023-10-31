51dd81011dc9b02e14f02a05655948532f16e014
JuneSunAt7 <jprogram@bk.ru>
2023-10-31 16:24:27 +0300

    NEW UI

--- cmd/client/client_interface.go
+++ cmd/client/client_interface.go
@@ -1,89 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"image/color"
+	"log"
 	"os"
 
-	client "github.com/EwRvp7LV7/45586694crypto/1client"
-	"github.com/charmbracelet/bubbles/table"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"gioui.org/app"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
 )
 
-var tableStyle = lipgloss.NewStyle().
-	BorderStyle(lipgloss.RoundedBorder()).
-	BorderForeground(lipgloss.Color("240"))
-
-type modelTable struct {
-	table table.Model
+func main() {
+	go func() {
+		w := app.NewWindow()
+		err := run(w)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
 }
 
-func (m modelTable) Init() tea.Cmd { return nil }
+func run(w *app.Window) error {
+	th := material.NewTheme()
+	var ops op.Ops
+	for {
+		e := <-w.Events()
+		switch e := e.(type) {
+		case system.DestroyEvent:
+			return e.Err
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
 
-func (m modelTable) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "esc":
-			if m.table.Focused() {
-				m.table.Blur()
-			} else {
-				m.table.Focus()
-			}
-		case "q", "ctrl+c":
-			return m, tea.Quit
-		case "enter":
-			client.Run(m.table.SelectedRow()[0])
+			title := material.H1(th, "Hello, Gio")
+			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
+			title.Color = maroon
+			title.Alignment = text.Middle
+			title.Layout(gtx)
+
+			e.Frame(gtx.Ops)
 		}
 	}
-	m.table, cmd = m.table.Update(msg)
-	return m, cmd
-}
-w
-func (m modelTable) View() string {
-	return tableStyle.Render(m.table.View()) + "\n"
-}
-
-func MainWindow() {
-	columns := []table.Column{
-		{Title: "#", Width: 4},
-		{Title: "Команда", Width: 15},
-		{Title: "Описание", Width: 30},
-	}
-
-	rows := []table.Row{
-		{"#1", "Скачать", "Скачивание файла с хранилища"},
-		{"#2", "Загрузить", "Загрузка в хранилище"},
-		{"#3", "Аутефикация", "Пароли и 2FA"},
-		{"#4", "Список", "Список файлов на сервере"},
-	}
-
-	t := table.New(
-		table.WithColumns(columns),
-		table.WithRows(rows),
-		table.WithFocused(true),
-		table.WithHeight(4),
-	)
-
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("#4103fc")).
-		Bold(false)
-	t.SetStyles(s)
-
-	m := modelTable{t}
-	if _, err := tea.NewProgram(m).Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
-	}
-}
-func main() {
-	MainWindow()
 }
--- go.mod
+++ go.mod
@@ -4,21 +4,30 @@ go 1.17
 
 require (
 	github.com/charmbracelet/lipgloss v0.9.1
-	github.com/google/uuid v1.3.0
+	github.com/google/uuid v1.4.0
 )
 
 require (
+	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
+	gioui.org/shader v1.0.8 // indirect
 	github.com/charmbracelet/bubbletea v0.24.1 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
+	github.com/go-text/typesetting v0.0.0-20230803102845-24e03d8b5372 // indirect
+	github.com/gotk3/gotk3 v0.6.2 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/maxence-charriere/go-app/v9 v9.8.0 // indirect
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
+	golang.org/x/exp v0.0.0-20221012211006-4de253d81b95 // indirect
+	golang.org/x/exp/shiny v0.0.0-20220827204233-334a2380cb91 // indirect
+	golang.org/x/image v0.5.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/term v0.6.0 // indirect
-	golang.org/x/text v0.3.8 // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
 
 require (
+	gioui.org v0.3.1
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/bubbles v0.16.1
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
