b71aa0640a3f67c75f469506d50e9a9ac998fc27
JuneSunAt7 <jprogram@bk.ru>
2023-11-30 05:25:05 +0300

    AUTORESERVING

--- 1client/reserving.go
+++ 1client/reserving.go
@@ -1,11 +1,11 @@
 package client
 
 func calendar() {
-
+	//TODO #4 Add calendar widget
 }
 func userfiles() {
-
+	// TODO #5 Add change directory
 }
 func setting() {
-
+	// TODO #6 It's include set directories, manage calendar and container files
 }
